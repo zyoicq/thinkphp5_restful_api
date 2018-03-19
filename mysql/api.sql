cfee28a03018af54e1c07a43bcda3fcdadeb96f0
zhangy <zy123zhangyong@163.com>
2018-03-19 23:49:34 +0800

    update sql

---
+++ mysql/api.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 5.5.53, for Win32 (AMD64)
+--
+-- Host: localhost    Database: api
+-- ------------------------------------------------------
+-- Server version	5.5.53
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `api_article`
+--
+
+DROP TABLE IF EXISTS `api_article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `api_article` (
+  `article_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `article_title` varchar(255) NOT NULL,
+  `article_uid` int(11) unsigned NOT NULL,
+  `article_content` text NOT NULL,
+  `article_ctime` int(11) NOT NULL,
+  PRIMARY KEY (`article_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `api_article`
+--
+
+LOCK TABLES `api_article` WRITE;
+/*!40000 ALTER TABLE `api_article` DISABLE KEYS */;
+INSERT INTO `api_article` VALUES (1,'title1',2,'',1521473242),(2,'title2',3,'',1521473298),(3,'title3',4,'',1521473352);
+/*!40000 ALTER TABLE `api_article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `api_user`
+--
+
+DROP TABLE IF EXISTS `api_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `api_user` (
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_phone` char(11) NOT NULL,
+  `user_nickname` varchar(255) NOT NULL COMMENT '昵称',
+  `user_email` varchar(255) NOT NULL,
+  `user_rtime` int(11) NOT NULL COMMENT 'register time',
+  `user_pwd` char(32) NOT NULL,
+  `user_icon` varchar(255) NOT NULL COMMENT '用户图像',
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `api_user`
+--
+
+LOCK TABLES `api_user` WRITE;
+/*!40000 ALTER TABLE `api_user` DISABLE KEYS */;
+INSERT INTO `api_user` VALUES (2,'13671178137','zhangy1','zy123_zy123@163.com',1333333333,'',''),(3,'','','zy123zhangyong@163.com',0,'25D55AD283AA400AF464C76D713C07AD',''),(4,'13671267603','','',1520953183,'25D55AD283AA400AF464C76D713C07AD','/uploads/20180314/e27e366d7801688c1d0328be80bc5859.png');
+/*!40000 ALTER TABLE `api_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-19 23:48:04
